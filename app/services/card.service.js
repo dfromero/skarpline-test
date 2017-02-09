751813e0f08f9d54f030dcdefad76e510d62956e
Diego Romero <romero.pozo2012@gmail.com>
2017-02-08 23:21:04 -0500

    update colors, fonts, styles

--- app/services/card.service.js
+++ app/services/card.service.js
@@ -8,17 +8,43 @@ angular.module('skarpline-test.services')
         CardService.getCards = function () {
             return $q.when([
                 {
+                    title: 'E-books',
+                    ownerPicture: 'http://pipsum.com/40x40.jpg'
+                },
+                {
+                    title: 'Landing GIFs',
+                    ownerPicture: 'http://pipsum.com/40x40.jpg'
+                },
+                {
+                    name: 'LANDING PAGE',
+                    ownerPicture: 'http://pipsum.com/40x40.jpg'
+                },
+                {
+                    name: 'New Social Image',
+                    ownerPicture: 'http://pipsum.com/40x40.jpg'
+                },
+                {
+                    name: 'Social Images',
+                    ownerPicture: 'http://pipsum.com/40x40.jpg'
+                },
+                {
                     name: 'Note name',
+                    ownerPicture: 'http://pipsum.com/40x40.jpg',
+                    note: true,
                     cardModifier: 'card-type-1',
                     htmlContent: ''
                 },
                 {
                     name: 'Note name',
+                    ownerPicture: 'http://pipsum.com/40x40.jpg',
+                    note: true,
                     cardModifier: 'card-type-2',
                     htmlContent: ''
                 },
                 {
                     name: 'Note name',
+                    ownerPicture: 'http://pipsum.com/40x40.jpg',
+                    note: true,
                     cardModifier: 'card-type-3',
                     htmlContent: ''
                 }
